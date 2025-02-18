e7745eaada8a260c97825e6fa0c68256ad6910a0
Igor Kudymov <koodimov@gmail.com>
2025-02-18 23:39:15 +0100

    fix find element in infinite sorted array

--- src/test/kotlin/binary_search/FindElementInInfiniteSortedArrayKtTest.kt
+++ src/test/kotlin/binary_search/FindElementInInfiniteSortedArrayKtTest.kt
@@ -14,6 +14,7 @@ class FindElementInInfiniteSortedArrayKtTest {
             row(intArrayOf(1, 3, 4, 6, 7, 9, 10, 12, 16, 19, 24, 32, 59, 105, 108, 204), 16, 8),
             row(intArrayOf(1, 3, 4, 6, 7, 9, 10, 12, 16, 19, 24, 32, 59, 105, 108, 204), 15, -1),
             row(intArrayOf(1, 3, 4, 6, 7, 9, 10, 12, 16, 19, 24, 32, 59, 105, 108, 204), 59, 12),
+            row(intArrayOf(1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 19), 3, 1)
         ) { nums, target, result ->
             findElementInInfiniteSortedArray(nums, target) shouldBe result
         }
