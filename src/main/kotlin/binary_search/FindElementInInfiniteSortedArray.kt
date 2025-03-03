bcf5fbb7c060623829a30b100d9476664adb5400
Igor Kudymov <koodimov@gmail.com>
2025-03-03 18:48:10 +0100

    simplify find element in infinite sorted array

--- src/main/kotlin/binary_search/FindElementInInfiniteSortedArray.kt
+++ src/main/kotlin/binary_search/FindElementInInfiniteSortedArray.kt
@@ -3,32 +3,24 @@ package mov.kudy.binary_search
 fun findElementInInfiniteSortedArray(nums: IntArray, target: Int): Int {
     var start = 0
     var end = 1
-    var chunkSize = 1
-    var answer = -1
+    var chunk = 1
 
-    while (start < nums.size && answer == -1) {
-        val startValue = nums[start]
-        val endValue = nums[end]
-        if (target in startValue..endValue) {
-            answer = binSearch(nums, start, end, target)
+    while (start < nums.size && nums[start] < target) {
+        if (nums[start] <= target && nums[end] >= target) {
+            return binSearch(nums, start, end, target)
         }
-        if (startValue > target) {
-            return -1
-        }
-
         start = end + 1
-        chunkSize *= 2
-        end = start + chunkSize
-        if (end > nums.size - 1) end = nums.size - 1
+        chunk *= 2
+        end = start + chunk
+        end = if (end < nums.size) end else nums.size - 1
     }
 
-    return answer
+    return -1
 }
 
 private fun binSearch(nums: IntArray, start: Int, end: Int, target: Int): Int {
     var start = start
     var end = end
-
     while (start <= end) {
         val mid = (start + end).shr(1)
         if (nums[mid] < target) {
@@ -37,6 +29,5 @@ private fun binSearch(nums: IntArray, start: Int, end: Int, target: Int): Int {
             end = mid - 1
         } else return mid
     }
-
     return -1
 }
